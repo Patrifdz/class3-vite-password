fdccfe0ff777eba53f30e2dd57778112f5fe9fd6
patrifdz <fdzpatri@gmail.com>
2024-10-15 21:06:46 +0200

    refactor: remove unused comment

--- src/password.model.ts
+++ src/password.model.ts
@@ -1,3 +1,2 @@
  export type Security = "low" | "medium" | "high";
 
-// export type LevelRegEx =  /^[a-z0-9]{6,10}$/ |  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,12}$/  |  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{12,16}$/;
