95c9b3ebfdf4361dcedf71b95b8865a6d11d6552
patrifdz <fdzpatri@gmail.com>
2024-10-19 08:58:52 +0200

    remove: three type variables of characters for each type of password

--- src/password.ts
+++ src/password.ts
@@ -1,5 +1,5 @@
 import type {Security} from './password.model';
-import {levelPassword, testPassword} from './password.aux';
+import { typePassword, testPassword } from './password.aux';
 
 const charactersLowPW = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9"; // -> 37 caracteres, solo minúsculas y números
 
@@ -8,13 +8,9 @@ const charactersMediumPW = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, 
 const charactersHighPW = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, Ñ, O, P, Q, R, S, T, U, V, W, X, Y, Z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, !, @, #, $, %, ^, &, *"; // -> 72 caracteres, minúsculas, mayúsculas, números y especiales
 
 
-export function getValidPassword ( regExOption : Security) : string {
+export function getValidPassword ( regExOption : Security, min : number, max: number) : string {
 
-    const level = levelPassword(regExOption);
-
-    const passwordRandom = "";
-
-    
+    const position 
 
     return "abc123"
 }
