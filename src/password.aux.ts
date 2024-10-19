a9df9055f61b1c8cf9816c95d7f3e1fe13857c3c
patrifdz <fdzpatri@gmail.com>
2024-10-19 08:25:39 +0200

    feat: add Password object instead of levelPassword function, and its interface 'Password' in password.model

--- src/password.aux.ts
+++ src/password.aux.ts
@@ -1,16 +1,36 @@
-import type { Security} from './password.model'
+import type { Password, Security } from "./password.model";
 
-export function levelPassword (level : Security) : RegExp  | string | undefined {
-    if (level === "low") {
-        return  /^[a-z0-9]{6,10}$/;
-    } else if (level === "medium") {
-        return  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,12}$/;
-    } else if (level === "high") {
-        return  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{12,16}$/;
-    }  else { "cannot get regular expression";}
+export const featurePassword : Password = {
+  low: {
+    regExp: /^[a-z0-9]{6,10}$/,
+    max: 10,
+    min: 6,
+  },
+  medium : {
+    regExp: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,12}$/,
+    max: 12,
+    min: 8
+  },
+  high: {
+    regExp : /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{12,16}$/,
+    max: 16,
+    min: 12,
+    }
+  }
+
+
+export function levelPassword(level: Security): RegExp | string | undefined {
+  if (level === "low") {
+    return /^[a-z0-9]{6,10}$/;
+  } else if (level === "medium") {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,12}$/;
+  } else if (level === "high") {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{12,16}$/;
+  } else {
+    ("cannot get regular expression");
+  }
 }
 
-export function testPassword (password : string, regEx : RegExp ) : boolean  {
-    return  regEx.test(password)
-    }
-
+export function testPassword(password: string, regEx: RegExp): boolean {
+  return regEx.test(password);
+}
--- src/password.model.ts
+++ src/password.model.ts
@@ -1,2 +1,13 @@
  export type Security = "low" | "medium" | "high";
 
+export interface Level  {
+    regExp : RegExp,
+    max: number,
+    min: number
+}
+
+export interface Password {
+    low : Level,
+    medium: Level,
+    high: Level
+}
