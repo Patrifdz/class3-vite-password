d278c3af4c68a3b763c672cf093d481081e56203
patrifdz <fdzpatri@gmail.com>
2024-10-19 08:42:26 +0200

    feat: add three variables for each type of password: low, medium and high

--- src/password.ts
+++ src/password.ts
@@ -1,7 +1,13 @@
 import type {Security} from './password.model';
 import {levelPassword, testPassword} from './password.aux';
 
-const capitalAlphabet : string = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, Ñ, O, P, Q, R, S, T, U, V, W, X, Y, Z." // -> 27 letras, generar número aleatorio entre 0 - 26 
+const charactersLowPW = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9"; // -> 37 caracteres, solo minúsculas y números
+
+const charactersMediumPW = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, Ñ, O, P, Q, R, S, T, U, V, W, X, Y, Z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9";  // -> 64 caracteres, minúsculas, mayúsculas y números
+
+const charactersHighPW = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, Ñ, O, P, Q, R, S, T, U, V, W, X, Y, Z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, !, @, #, $, %, ^, &, *"; // -> 72 caracteres, minúsculas, mayúsculas, números y especiales
+
+const capitalAlphabet : string = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, Ñ, O, P, Q, R, S, T, U, V, W, X, Y, Z" // -> 27 letras, generar número aleatorio entre 0 - 26 
 const minusAlphabet : string = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z"; // -> 27 letras, generar número aleatorio entre 0 - 26 
 
 const numbers : string = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9" // -> 10 números, generar número aleatorio entre 0 - 9
