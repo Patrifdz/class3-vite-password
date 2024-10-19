d3fd518a0e02b610e4ed3a9fe899effd37952ad9
patrifdz <fdzpatri@gmail.com>
2024-10-19 08:57:16 +0200

    feat: add property characters to typePassword object

--- src/password.aux.ts
+++ src/password.aux.ts
@@ -1,20 +1,23 @@
 import type { Password, Security } from "./password.model";
 
-export const featurePassword : Password = {
+export const typePassword : Password = {
   low: {
     regExp: /^[a-z0-9]{6,10}$/,
     max: 10,
     min: 6,
+    characters: "a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9",
   },
   medium : {
     regExp: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,12}$/,
     max: 12,
-    min: 8
+    min: 8, 
+    characters: "a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, Ñ, O, P, Q, R, S, T, U, V, W, X, Y, Z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9",
   },
   high: {
     regExp : /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{12,16}$/,
     max: 16,
     min: 12,
+    characters: "a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, Ñ, O, P, Q, R, S, T, U, V, W, X, Y, Z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, !, @, #, $, %, ^, &, *",
     }
   }
 
