01af2faae5fae4cd028e84a132d4a5e0f4e2fc0e
patrifdz <fdzpatri@gmail.com>
2024-10-19 08:31:52 +0200

    remove: function 'levelPassword'

--- src/password.aux.ts
+++ src/password.aux.ts
@@ -19,18 +19,6 @@ export const featurePassword : Password = {
   }
 
 
-export function levelPassword(level: Security): RegExp | string | undefined {
-  if (level === "low") {
-    return /^[a-z0-9]{6,10}$/;
-  } else if (level === "medium") {
-    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,12}$/;
-  } else if (level === "high") {
-    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{12,16}$/;
-  } else {
-    ("cannot get regular expression");
-  }
-}
-
 export function testPassword(password: string, regEx: RegExp): boolean {
   return regEx.test(password);
 }
